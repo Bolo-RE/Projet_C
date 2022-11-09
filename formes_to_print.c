6e3c7d58644f262881f43ca65bc743446f51fa3c
bololo7 <101951542+bololo7@users.noreply.github.com>
2022-11-09 14:44:27 +0100

    Formes

---
+++ formes_to_print.c
@@ -0,0 +1,7 @@
+/* Print des formes carrés pour les centrales, cercles pour les villes et traçage des 
+lignes reliant les villes et les centrales. Les lignes seront de la couleur de la centrale */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
