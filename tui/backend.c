cb2377d64f1c1fe1a08313767d915858034af64e
p.gsd <pgsd@disroot.org>
2022-12-03 10:55:34 +0100

    Implementing the network view, fixed some minor bugs

---
+++ tui/backend.c
@@ -0,0 +1,382 @@
+//Goussard Philemon et Iglesias Guillaume
+//Projet electricite GIF 2022 
+//Wed Oct 19 02:17:08 PM CEST 2022
+
+// TODO Replace all the printfs by log function
+// TODO If time; add name to the centrale structure
+// TODO Setup save centrales pointer to be passed to the other functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LOGFILE "dump.log"
+
+struct ville {
+  int code;
+  char* name;
+  struct ville* suivant;
+  int y;
+};
+
+struct ligne {
+  int id;
+  int puissance;
+  struct ville* ville;
+  struct ligne* suivant;
+};
+
+struct centrale {
+  int id;
+  struct ligne* lignes;
+  struct centrale* suivant;
+  struct centrale* prev;
+};
+
+// Contient le pointeur vers la premiere ville
+static unsigned long p_ville;
+
+int logmsg(char* message){
+  FILE* fp = fopen(LOGFILE, "w");
+  if(fp == NULL){
+    return -1;
+  }
+  // TODO Find out in which situation this can SEGV and how to prevent that
+  char* dump = malloc(strlen(message)*sizeof(char)+200);
+  strcpy(dump,message);
+  fputs(dump, fp);
+  fclose(fp);
+  free(dump);
+  return 0;
+}
+
+void set_p_ville(unsigned long ptr){
+  p_ville = ptr;
+}
+
+struct ville* get_p_ville(){
+  return (struct ville*) p_ville;
+}
+
+struct ville* get_ville(int index){
+  struct ville* ville = get_p_ville();
+  while(ville != NULL){
+    if (ville->code == index){
+      return ville;
+    }
+     ville = ville->suivant;   
+  }
+  return NULL;
+}
+
+int reset_ville_position(){
+  struct ville* villes = get_p_ville();
+  while(villes != NULL){
+    villes->y = -1;
+    villes = villes->suivant;
+  }
+  return 0;
+}
+
+
+// Ajouter / Retirer ville
+int add_ville(int code, char* name){
+  // printf("name is %s\n", name);
+  struct ville* next = get_p_ville();
+  while(next->suivant != NULL){
+    next = next->suivant;
+  }
+  struct ville* temp = (struct ville*) malloc(sizeof(struct ville));
+  if(temp == NULL){
+    return -1;
+  }
+  // This code is reserved to identify the first link and shall not be used elsewhere
+  if(next->code==0){
+    next->name = name;
+    next->code = code;
+    //Spot value for the drawing functions
+    next->y = -1;
+  }else{
+    temp->name = name;
+    temp->code = code;
+    temp->y = -1;
+    next->suivant = temp;
+  }
+  return 0;
+}
+
+int rm_ville(int code){
+  struct ville* p = get_p_ville();
+  struct ville** ville = &p;
+  // TODO Free to setup
+  while((*ville)->suivant != NULL){
+    if ((*ville)->suivant->code == code){
+      if((*ville) == get_p_ville()){
+        logmsg("removing the first node; changing the value of p_ville");
+        set_p_ville((unsigned long)(*ville)->suivant);
+      }
+      if ((*ville)->suivant->suivant == NULL){
+        (*ville)->suivant = NULL;
+      }else{
+        (*ville)->suivant = (*ville)->suivant->suivant;
+      }
+      return 0;
+    }
+    else{
+      (*ville) = (*ville)->suivant;
+    }
+  }
+  return -1;
+}
+
+
+
+struct centrale* get_centrale(struct centrale* centrales, int id){
+  struct centrale* res = NULL;
+  while(centrales->prev != NULL){
+    centrales = centrales->prev;
+  }
+  while(centrales != NULL){
+    if(centrales->id == id){
+      if (res == NULL){
+        res = centrales;        
+      }
+      else{
+        logmsg("Found double value ! ID should be unique, else undefined behavoir can happen. You have been warned.\n");
+      }
+    }
+    centrales = centrales->suivant;
+  }
+  return res;
+}
+// Ajouter / Retirer centrale
+int add_centrale(struct centrale** centrale, int id){
+  // Checking centrale is at the last position
+  while((*centrale)->suivant != NULL){
+    (*centrale) = (*centrale)->suivant;
+  }
+  struct centrale* temp = (struct centrale*) malloc(sizeof(struct centrale));
+  temp->id = id;
+  // Useful to identify when lignes is empty
+  temp->lignes = NULL;
+  temp->prev = (*centrale);
+  (*centrale)->suivant = temp;
+  while((*centrale)->prev !=NULL){
+    (*centrale) = (*centrale)->prev;
+  }
+  return 0;
+}
+
+int rm_centrale(struct centrale** centrale, int id){
+  // Checking the centrale argument is at position 0
+  while((*centrale)->prev != NULL){
+    (*centrale) = (*centrale)->prev;
+  }
+  // Start looking for value
+  while((*centrale)->suivant != NULL){
+    if((*centrale)->suivant->id == id){
+      if ((*centrale)->suivant == NULL){
+        (*centrale)->suivant = NULL;
+      }else{
+      (*centrale)->suivant = (*centrale)->suivant->suivant;
+      (*centrale)->suivant->prev = (*centrale);
+      }
+    }
+  }
+  return 0;
+}
+// Ajouter / Retirer ligne
+int add_ligne(struct centrale* centrale, int puissance, struct ville* ville){
+  if (ville == NULL){
+    printf("Issue with ville argument : NULL value\n");
+    return -1;
+  }
+  struct ligne* lignes = centrale->lignes;
+  if(lignes == NULL){
+    lignes = (struct ligne*) malloc(sizeof(struct ligne));
+  }
+  while(lignes->suivant != NULL){
+    lignes = lignes->suivant;
+  }
+  struct ligne* temp = (struct ligne*) malloc(sizeof(struct ligne));
+  temp->puissance = puissance;
+  temp->ville = ville;
+  lignes->suivant = temp;
+  if(centrale->lignes == NULL){
+    centrale->lignes = temp;
+  }else{
+    centrale->lignes = lignes;
+  }
+  return 0;
+}
+int rm_ligne(struct centrale* centrale, int id){
+  struct ligne* lignes = centrale->lignes;
+  while(lignes->suivant != NULL){
+    if (lignes->suivant->id == id){
+      if (lignes->suivant->suivant == NULL){
+        lignes->suivant = NULL;
+      }else{
+        lignes->suivant = lignes->suivant->suivant;
+      }
+    }
+    else{
+      lignes= lignes->suivant;
+    }
+  }
+  return 0;
+}
+// Modification de la puissance d'une centrale
+void change_power(struct centrale* centrale, int puissance){
+  struct ligne* lignes = centrale->lignes;
+  while(lignes != NULL){
+    lignes->puissance = puissance;
+    lignes = lignes->suivant;
+  }
+}
+// Recuperer la valeur de puissance d'une ville et la repartition / centrale
+void power_display(struct centrale* centrales, int code_ville){
+  int p_total = 0;
+  while(centrales->suivant != NULL){
+    
+    struct ligne* lignes = centrales->lignes;
+    while(lignes->suivant != NULL){
+      if(lignes->ville->code == code_ville){
+        printf("La centrale %d apporte %d energie a la ville %d", centrales->id, lignes->puissance, code_ville);  
+        // TODO how to print this information on the screen ?
+        p_total += lignes->puissance;
+      }
+      lignes= lignes->suivant;
+    }
+    centrales = centrales->suivant;  
+  }
+  printf("Au total, la ville %d recoit %d energie", code_ville, p_total);
+}
+
+// TODO For the moment the last saved file is wiped and we write everything back ; maybe thing of append?
+void save(struct centrale* centrales,  char* fichier){
+  FILE* fp  = fopen(fichier, "w");
+  char counter = '0';
+  char* base = "network.bck";
+  while(fp == NULL){
+    strcat(strdup(base), &counter);
+    fp = fopen(base, "w");
+    counter++;
+  }
+  struct ville* villes = get_p_ville();
+  printf("test\n");
+  fputs("#VILLES\n", fp);
+  
+  while(villes != NULL){
+    fprintf(fp, "%d:%s\n", villes->code, villes->name);
+    villes = villes->suivant;
+  }
+  fputs("#FINVILLE\n#CENTRALE\n", fp);
+  while(centrales != NULL){
+    fprintf(fp, "%d\n", centrales->id);
+    printf("lignes : %p\n", centrales->lignes);
+    if(centrales->lignes == NULL){
+      puts("no lignes\n");
+      centrales = centrales->suivant;
+      continue;
+    }
+    fprintf(fp, "#LIGNE\n");
+    struct ligne* ligne = centrales->lignes;
+    while(ligne != NULL){
+      printf(" code %d\n", ligne->ville->code);
+      fprintf(fp, "%d:%d\n", ligne->ville->code, ligne->puissance);
+      ligne = ligne->suivant;
+    }
+    fprintf(fp, "#FINLIGNE\n");
+    centrales = centrales->suivant;
+  }
+  fputs("#FINCENTRALE",fp);
+  fclose(fp);
+}
+
+// The centrale argument is not used, only here for homogenity
+int load_ville(char* data, struct centrale* padding){
+  int id;
+  char* idv  = strtok(data, ":");
+  char* name = strtok(NULL, ":");
+  if(name == NULL){
+    // printf("%s\n", idv);
+    return -1;
+  }else{
+    // printf("created struct ville with values %s and %s\n", idv, name);
+    id = strtol(idv, (char**)NULL,  10);
+    // printf("created struct ville with values %d and %s \n", id, name);
+    add_ville(id, strdup(name));
+  }
+  return 0;  
+}
+
+int load_centrale(char* data, struct centrale* chain){
+  int id;
+  // Check if line only contains what we need -> an int
+  if(strlen(data)>2){
+    // printf("str %s\n", data);
+    return -1;
+  }
+  id = strtol(data, (char**)NULL, 10);
+  printf("%d\n",id);
+  add_centrale(&chain, id);
+  return 0;  
+}
+
+int load_ligne(char* data, struct centrale* centrale){
+  int id;
+  int power;
+  char* idv  = strtok(data, ":");
+  char* powerv = strtok(NULL, ":");
+  if(powerv == NULL){
+    return -1;
+  }else{
+    // printf("created struct ville with values %s and %s\n", idv, name);
+    id = strtol(idv, (char**)NULL,  10);
+    power = strtol(powerv, (char**)NULL, 10);
+    printf("created struct ligne with values %d and %d in centrale %d\n", id, power, centrale->id);
+    add_ligne(centrale, power, get_ville(id));
+  }
+  return 0;  
+}
+
+// TODO Charger le reseau depuis un fichier
+void load(char* filename, struct ville* villes, struct centrale* centrales){
+  if(filename == NULL)
+    filename = "network.bck";
+  FILE* fp;
+  fp = fopen(filename, "r");
+  if(fp==NULL){
+    printf("Error while opening file %s\n", filename);
+    return;
+  }
+  char line[100];
+  int steps = 0;
+  int (*loaders[3])(char*, struct centrale*) = {load_ville, load_centrale, load_ligne};
+  char* name;
+  int id;
+  int power;
+  int central_id = 0;
+  while(fgets(line, sizeof(line), fp)!=NULL){
+    if(strcmp(line, "#FINVILLE\n") == 0){
+      steps = 1;
+    }
+    if(strcmp(line, "#LIGNE\n") == 0){
+      steps = 2;
+    }
+    if(strcmp(line, "#FINLIGNE\n") == 0){
+      steps = 1;
+      central_id++;
+    }
+
+    if(strcmp(line, "#FINCENTRALE\n") == 0){
+      return;
+    }
+    // TODO Error handling
+    if(get_centrale(centrales, central_id)== NULL){
+      loaders[steps](line, centrales);
+    }else{
+      loaders[steps](line, get_centrale(centrales, central_id));
+    }
+  }
+  fclose(fp);
+}
+
--- tui/tui.c
+++ tui/tui.c
@@ -1,4 +1,5 @@
 #include "protocol.h"
+#include "backend.c"
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -8,6 +9,8 @@
 #define COLONNES 104
 #define OFFSET 2
 #define PADDING "\033[22;34m|"
+#define XCENTRALE 4
+#define XVILLE 80
 struct func {
   char* name;
   int (*fun)();
@@ -20,6 +23,10 @@ struct func {
 // TODO Input handling (best result function)
 // TODO Options read from backup file
 // TODO Fix keymap
+// TODO View everything
+// TODO View one particular node of network
+// TODO Error code handling
+// TODO Log clearing at starting
 int baroffset = 0;
 void draw(void);
 int x,y;
@@ -28,7 +35,7 @@ int drawtoolbar(int n);
 int incrtoolbar(void);
 int dincrtoolbar(void);
 void permtoolbar(void);
-void drawnetwork(void);
+// void drawnetwork(struct centrale*);
 int test1(void);
 int test2(void);
 int test3(void);
@@ -96,7 +103,6 @@ int drawtoolbar(int n){
   return 0;
 }
 
-
 // TODO Should return the best match in whole name functions
 char* getfuncname(char* part){
   int index = 0;
@@ -107,6 +113,9 @@ char* getfuncname(char* part){
 
 void showWarning(char* message){
   // Show message on the middle of the screen; need to save prev state show the message and 
+  locate(LIGNES/2, COLONNES/2);
+  anykey(message);
+  cls();
   // clear on return key
 }
 
@@ -115,36 +124,78 @@ char* getinput(void){
   locate(0,27);
   int x = 0;
   // Should be enough
+  // TODO Fix the fact that keystrokes are evaluated one loop after
   char input[100];
   while(1){
-    if(kbhit()){
+    // Keeping it maybe useful in Windows but crashing everything in Linux
+    // if(kbhit()){
       char k = getkey();
       if(k == KEY_ENTER){
         return strdup(input);
       }
       // numpad1 is backspace in Linux
       // TODO Check if this is the case for Windows
+      // This needs to be implemented under Windows;; too much !=
       else if(k == KEY_NUMPAD1){
-        // TODO Implement check min
-        input[strlen(input)-1 == 0 ? strlen(input-1) : 0] = '\0';
+        //Remove a char
+        input[strlen(input)-1] = 0;
         locate(x, 27);
         setChar(' ');
         x  =  x == 0 ? x : x - 1;
       }
+      // Include numbers
       else if ('A' <= k && k <= 'z'){
-        if(x <= 100){
+        if(x >= 100){
           showWarning("Ooops... Seems command you're typing is a bit too long");
         }
         strcat(input, &k);
-        printf("%c", k);
+        locate(x, 27);
+        setChar(k);
         x++;
       }
-      else{
-        printf("%d\n", k);
-      }
+      // else{
+       // locate(0,0);
+        // printf("%d\n", k);
+        // locate(27,x);
+      // }
     }
-  }
+  // }
 }
+
+
+//TODO Moving around could be implemented but not mandatory for the moment
+//TODO Choose the id to draw 
+void drawnetwork(struct centrale* centrales){
+  locate(XCENTRALE,1);
+  int ycentrale = 1;
+  int yville = 1;
+  //TODO Take in account that may be needed at the end of file
+  while(centrales != NULL && ycentrale <= LIGNES && yville <= LIGNES){
+    setChar('C');
+    struct ligne* lignes = centrales->lignes;
+    while(lignes != NULL){
+      if(lignes->ville->y == -1){
+        lignes->ville->y = yville;
+        locate(XVILLE, yville);
+        setChar(lignes->ville->name[0]);
+        yville++;
+      }
+      lignes = lignes->suivant;
+    }
+    centrales = centrales->suivant;
+    locate(XCENTRALE,ycentrale);
+    ycentrale++;
+  }
+  // Blue is for centrale; 
+  // White is for the town;
+  // Trying to use the most different colors for the link
+}
+
+// void drawnode(struct centrale* centrales){
+//   // Blue is for centrale; 
+//   // White is for the town;
+//   // Trying to use the most different colors for the link
+// }
 
 void init(void){
   cls();
@@ -152,7 +203,6 @@ void init(void){
   // drawtoolbar(1);
   if(BORDER){
     for(int i =0; i < LIGNES;i++){
-      // TODO Add color
       printXY(0,i, PADDING);
       printXY(COLONNES,i, PADDING);
     }
@@ -182,22 +232,40 @@ struct func getfnc(char map){
 }
 
 int main(void){
+
+  logmsg("---------------------------------------------");
+  struct centrale* centrales = malloc(sizeof(struct centrale));
+  struct ville* villes = malloc(sizeof(struct ville));
+  set_p_ville((unsigned long)villes);
+  add_ville(1, "test");
+  add_ville(2, "test");
+  add_ville(3, "test");
+  add_ville(4, "test");
+  add_centrale(&centrales, 1);
+  add_centrale(&centrales, 2);
+  add_centrale(&centrales, 3);
+  add_centrale(&centrales, 4);
+  add_ligne(get_centrale(centrales, 1), 120, get_ville(1));
+  add_ligne(get_centrale(centrales, 2), 120, get_ville(4));
+  add_ligne(get_centrale(centrales, 3), 120, get_ville(3));
+  // add_ligne(get_centrale(centrales, 1), 120, get_ville(2));
   hidecursor();
   saveDefaultColor();
-  init();
-  // colorPrint(RED, NOCOLOR, "test");
+  // init();
+  cls();
+  drawnetwork(centrales);
   // draw();
-  while(1){
-    if(kbhit()){
-      char k = getkey();
-      struct func f = getfnc(k);
-      if(strcmp(f.name,"NONE") == 0){
-        // TODO Call the showhelp function
-        printf("non mapped");
-        continue;
-      }
-      // Pray pointer is pointing to some function
-      f.fun();
-    }
-  }
+  // while(1){
+  //   if(kbhit()){
+  //     char k = getkey();
+  //     struct func f = getfnc(k);
+  //     if(strcmp(f.name,"NONE") == 0){
+  //       // TODO Call the showhelp function
+  //       printf("non mapped");
+  //       continue;
+  //     }
+  //     // Pray pointer is pointing to some function
+  //     f.fun();
+  //   }
+  // }
 }
